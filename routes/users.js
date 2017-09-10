064536dbd1ad8b5cd379a7aceee8f40575bc8d95
Damir Bogdanov <yespeaceee@gmail.com>
2017-09-10 22:40:04 +1000

    Part 7

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,24 @@
-var express = require('express');
-var router = express.Router();
+  var express = require('express');
+  var router = express.Router();
 
-/* GET userlist */
-router.get('/userlist', function(req, res) {
+  /* GET userlist */
+  router.get('/userlist', function(req, res) {
+      var db = req.db;
+      var collection = db.get('userlist');
+      collection.find({},{},function(e,docs){
+          res.json(docs);
+      });
+  });
+
+  /* POST to adduser */
+  router.post('/adduser', function(req, res){
     var db = req.db;
     var collection = db.get('userlist');
-    collection.find({},{},function(e,docs){
-        res.json(docs);
+    collection.insert(req.body, function(err, result){
+      res.send(
+          (err === null) ? { msg: ''} : { msg:err }
+      );
     });
-});
+  });
 
-module.exports = router;
+  module.exports = router;
