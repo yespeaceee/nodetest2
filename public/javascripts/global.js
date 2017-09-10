3f8528fa04eaa885b9f8adafa0d59071262c02e9
Damir Bogdanov <yespeaceee@gmail.com>
2017-09-10 21:33:25 +1000

    Part 6

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -7,6 +7,9 @@ $(document).ready(function(){
     // populate the user table on initial page load
 	populateTable();
 	
+	// Username link click
+	$('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+	
 });
 
 // === Functions ===
@@ -20,6 +23,9 @@ function populateTable(){
 	// jQuery AJAX calls for JSON
 	$.getJSON( '/users/userlist', function( data ){
 		
+		// Stick user data array into a userlist variable in the global object
+		userListData = data;
+		
 		// For each item in JSON, add a table row and cells to the content string
 		$.each(data, function(){
 			tableContent += '<tr>';
@@ -32,4 +38,26 @@ function populateTable(){
 		// inject the whole content string into existing html table
 		$('#userList table tbody').html(tableContent);
 	});
+};
+
+// Show user info
+function showUserInfo(event) {
+	
+	// Prevent Link from firing
+	event.preventDefault();
+	
+	// Retrieve username from link rel attribute
+	var thisUserName = $(this).attr('rel');
+	
+	// Get index of object based on id value
+	var arrayPosition = userListData.map(function(arrayItem) {return arrayItem.username; }).indexOf(thisUserName);
+	
+	// Get User Object
+	var thisUserObject = userListData[arrayPosition];
+	
+	// Populate info box
+	$('#userInfoName').text(thisUserObject.fullname);
+	$('#userInfoAge').text(thisUserObject.age);
+	$('#userInfoGender').text(thisUserObject.gender);
+	$('#userInfoLocation').text(thisUserObject.location);
 };
